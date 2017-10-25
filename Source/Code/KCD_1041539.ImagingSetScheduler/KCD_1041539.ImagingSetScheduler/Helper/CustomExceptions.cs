daea82ce987b443a7ff45205696c1c7265124ad3
Bill Harper <bill.harper@relativity.com>
2017-10-25 14:42:28 -0500

    revert customexceptions, update version requirement

--- Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Helper/CustomExceptions.cs
+++ Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Helper/CustomExceptions.cs
@@ -5,6 +5,11 @@
 		[System.Serializable]
 		public class ImagingSetSchedulerException : System.Exception
 		{
+			public ImagingSetSchedulerException()
+				: base()
+			{
+			}
+
 			public ImagingSetSchedulerException(string message)
 				: base(message)
 			{
@@ -12,6 +17,12 @@
 
 			public ImagingSetSchedulerException(string message, System.Exception inner)
 				: base(message, inner)
+			{
+			}
+
+			// A constructor is needed for serialization when an
+			// exception propagates from a remoting server to the client. 
+			protected ImagingSetSchedulerException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
 			{
 			}
 		}
