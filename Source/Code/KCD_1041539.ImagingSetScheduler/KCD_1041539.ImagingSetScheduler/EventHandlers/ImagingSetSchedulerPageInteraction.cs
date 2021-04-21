e5e4b90fdf2570432c79a9a6bf610727ce4e8136
Yahir De Leon <yahir.deleon@relativity.com>
2021-04-21 10:03:28 -0500

    REL-546690 Conditionally show ISS deprecation message

--- Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/EventHandlers/ImagingSetSchedulerPageInteraction.cs
+++ Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/EventHandlers/ImagingSetSchedulerPageInteraction.cs
@@ -11,7 +11,7 @@ namespace KCD_1041539.ImagingSetScheduler.EventHandlers
 		{
 			Response retVal = new Response {Success = true, Message = string.Empty};
 
-			string warningMessage = "WARNING: Test";
+			string warningMessage = BuildDeprecationMessage();
 
 			string htmlToInsert = "<script type=\"text/javascript\"> " +
 								  "var messageContainer = window.document.createElement(\"tr\");" +
@@ -27,6 +27,69 @@ namespace KCD_1041539.ImagingSetScheduler.EventHandlers
 			});
 
 			return retVal;
-        }
+		}
+
+		private string BuildDeprecationMessage()
+		{
+			const string sql = "SELECT [Value] FROM [Relativity] WHERE [Key] = 'Version'";
+			string currentReleaseVersion = Helper.GetDBContext(-1).ExecuteSqlStatementAsScalar<string>(sql);
+			const string prairieSmokeVersion = "12.2";
+			bool isR1Instance = IsCloudInstanceEnabled();
+
+			string deprecationMessage;
+
+			if (isR1Instance && IsIncomingVersionHigherOrEqual(prairieSmokeVersion, currentReleaseVersion))
+			{
+				//test
+				deprecationMessage = "Imaging Set Scheduler is now obsolete and disabled. Check out Imaging Automated Workflows!";
+			}
+			else
+			{
+				deprecationMessage = string.Empty;
+			}
+
+			return deprecationMessage;
+		}
+
+		private bool IsCloudInstanceEnabled()
+		{
+			const string sql = "SELECT [Value] FROM [InstanceSetting] WHERE [Name] = 'CloudInstance' AND [Section] = 'Relativity.Core'";
+			string currentReleaseVersion = Helper.GetDBContext(-1).ExecuteSqlStatementAsScalar<string>(sql) ?? "false";
+			bool retVal;
+			if (!bool.TryParse(currentReleaseVersion.ToLower(), out retVal))
+			{
+				retVal = false;
+			}
+			return retVal;
+		}
+
+		private bool IsIncomingVersionHigherOrEqual(string targetVersion, string incomingVersion)
+		{
+			string[] targetVersionParts = targetVersion.Split('.');
+			string[] incomingVersionParts = incomingVersion.Split('.');
+
+			int maxIndex;
+
+			if (targetVersionParts.Length != incomingVersionParts.Length)
+			{
+				// stop at whatever the shortest is to prevent a out of bounds error
+				maxIndex = Math.Min(targetVersionParts.Length, incomingVersionParts.Length);
+			}
+			else
+			{
+				maxIndex = targetVersion.Length;
+			}
+
+			bool isVersionHigher = true;
+
+			// set flag to false when an incoming number is less than our target
+			for (int i = 0; i < maxIndex; i++)
+			{
+				int incomingVersionPart = Convert.ToInt32(incomingVersionParts[i]);
+				int targetVersionPart = Convert.ToInt32(targetVersionParts[i]);
+				isVersionHigher &= incomingVersionPart >= targetVersionPart;
+			}
+			return isVersionHigher;
+		}
 	}
 }
