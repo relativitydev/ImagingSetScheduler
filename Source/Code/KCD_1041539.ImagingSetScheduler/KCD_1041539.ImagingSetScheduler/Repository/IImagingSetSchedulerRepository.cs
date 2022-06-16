1f3788ee4d4ffb241086508528de60c17ba30503
Xitao Lv <xitao.lv@relativity.com>
2022-06-15 23:22:53 -0500

    REL-697978 RSPAI removal

---
+++ Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Repository/IImagingSetSchedulerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Relativity.Services.Objects.DataContracts;
+
+namespace KCD_1041539.ImagingSetScheduler.Repository
+{
+	public interface IImagingSetSchedulerRepository
+	{
+		Task<List<RelativityObject>> RetrieveAllImagingSetSchedulesNotWaitingAsync(int workspaceId);
+	}
+}
---
+++ Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Repository/ServicesProxyFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Relativity.API;
+
+namespace KCD_1041539.ImagingSetScheduler.Repository
+{
+	public class ServicesProxyFactory : IServicesProxyFactory
+	{
+		private readonly IServicesMgr _servicesManager;
+
+		public ServicesProxyFactory(IServicesMgr servicesManager)
+		{
+			_servicesManager = servicesManager;
+		}
+
+		public T CreateServiceProxyAsSystem<T>() where T : IDisposable
+		{
+			return _servicesManager.CreateProxy<T>(ExecutionIdentity.System);
+		}
+
+		public T CreateServiceProxy<T>() where T : IDisposable
+		{
+			return _servicesManager.CreateProxy<T>(ExecutionIdentity.CurrentUser);
+		}
+	}
+}
