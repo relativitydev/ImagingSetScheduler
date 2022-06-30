0f5f8b031e894b61b0d1936ab54326a968fc76f9
Marianne Hernandez <marianne.hernandez@relativity.com>
2022-06-30 13:46:23 -0500

    REL-693701 Add XML Documentation for IContextContainer.cs and IObjectManagerHelper.cs

--- Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Helper/IObjectManagerHelper.cs
+++ Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Helper/IObjectManagerHelper.cs
@@ -7,6 +7,12 @@ namespace KCD_1041539.ImagingSetScheduler.Helper
 {
     public interface IObjectManagerHelper
     {
-        Task<List<RelativityObject>> RetrieveAllImagingSetSchedulesNotWaitingAsync(int workspaceId, IContextContainer contextContainer);
+		/// <summary>
+		/// Retrieves Imaging Set Schedules whose status is not "Waiting" in a workspace.
+		/// </summary>
+		/// <param name="workspaceId">The Artifact ID of the workspace.</param>
+		/// <param name="contextContainer">The context container.</param>
+		/// <returns>A list of Imaging Set Schedules objects with Guid fields.</returns>
+		Task<List<RelativityObject>> RetrieveAllImagingSetSchedulesNotWaitingAsync(int workspaceId, IContextContainer contextContainer);
     }
 }
