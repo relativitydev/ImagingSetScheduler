0481bdd09abee63daf3e4a2424744a105627ecef
Marianne Hernandez <marianne.hernandez@relativity.com>
2022-07-28 14:30:10 -0500

    REL-711178 Add UpdateImagingSetScheduler to ObjectManagerHelper.cs. Also change variable names of in ObjectManagerHelper.cs for consistency.

--- Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Helper/IObjectManagerHelper.cs
+++ Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Helper/IObjectManagerHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using KCD_1041539.ImagingSetScheduler.Context;
 using Relativity.Services.Objects.DataContracts;
@@ -10,18 +11,31 @@ namespace KCD_1041539.ImagingSetScheduler.Helper
         /// <summary>
         /// Retrieves a single Imaging Set Schedule in a workspace.
         /// </summary>
-        /// <param name="workspaceId">The Artifact ID of the workspace.</param>
+        /// <param name="workspaceArtifactId">The Artifact ID of the workspace.</param>
         /// <param name="contextContainer">The context container.</param>
-        /// /// <param name="imagingSetSchedulerId">The Artifact ID of the desired Imaging Set Schedule.</param>
+        /// <param name="imagingSetSchedulerArtifactId">The Artifact ID of the desired Imaging Set Schedule.</param>
         /// <returns>An Imaging Set Schedule objects with Guid fields.</returns>
-        Task<RelativityObject> RetrieveSingleImagingSetScheduler(int workspaceId, IContextContainer contextContainer, int imagingSetSchedulerId);
+        Task<RelativityObject> RetrieveSingleImagingSetScheduler(int workspaceArtifactId, IContextContainer contextContainer, int imagingSetSchedulerArtifactId);
 
         /// <summary>
         /// Retrieves Imaging Set Schedules whose status is not "Waiting" in a workspace.
         /// </summary>
-        /// <param name="workspaceId">The Artifact ID of the workspace.</param>
+        /// <param name="workspaceArtifactId">The Artifact ID of the workspace.</param>
         /// <param name="contextContainer">The context container.</param>
         /// <returns>A list of Imaging Set Schedules objects with Guid fields.</returns>
-        Task<List<RelativityObject>> RetrieveAllImagingSetSchedulesNotWaitingAsync(int workspaceId, IContextContainer contextContainer);
+        Task<List<RelativityObject>> RetrieveAllImagingSetSchedulesNotWaitingAsync(int workspaceArtifactId, IContextContainer contextContainer);
+
+        /// <summary>
+        /// Updates the "Last Run" and "Next Run" fields for an Imaging Set Schedule, also updates "Messages" and "Status" for an Imaging Set Schedule.
+        /// </summary>
+        /// <param name="workspaceArtifactId">The Artifact ID of the workspace.</param>
+        /// <param name="contextContainer">The context container.</param>
+        /// <param name="imagingSetSchedulerArtifactId">The Artifact ID of the desired Imaging Set Schedule.</param>
+        /// <param name="lastRun">The last run of the Imaging Set Schedule.</param>
+        /// <param name="nextRun">The next run of the Imaging Set Schedule.</param>
+        /// <param name="message">Description of the update operation.</param>
+        /// <param name="status">The new status of the Imaging Set Schedule.</param>
+        /// <returns>A count of the objects updated, success status, and a description of the update.</returns>
+        Task<MassUpdateResult> UpdateImagingSetScheduler(int workspaceArtifactId, IContextContainer contextContainer, int imagingSetSchedulerArtifactId, DateTime? lastRun, DateTime? nextRun, string message, string status);
     }
 }
--- Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Helper/ObjectManagerHelper.cs
+++ Source/Code/KCD_1041539.ImagingSetScheduler/KCD_1041539.ImagingSetScheduler/Helper/ObjectManagerHelper.cs
@@ -11,14 +11,14 @@ namespace KCD_1041539.ImagingSetScheduler.Helper
     {
         public readonly Guid IMAGING_SET_SCHEDULER = new Guid("45C9FEB9-43D8-43FE-A216-85B1F062B0A7");
 
-        public async Task<RelativityObject> RetrieveSingleImagingSetScheduler(int workspaceId, IContextContainer contextContainer, int imagingSetSchedulerId)
+        public async Task<RelativityObject> RetrieveSingleImagingSetScheduler(int workspaceArtifactId, IContextContainer contextContainer, int imagingSetSchedulerArtifactId)
         {
-            if (workspaceId < 0)
+            if (workspaceArtifactId < 0)
             {
                 throw new ArgumentException(Constant.ErrorMessages.WORKSPACE_ARTIFACT_ID_CANNOT_BE_NEGATIVE);
             }
 
-            if (imagingSetSchedulerId < 0)
+            if (imagingSetSchedulerArtifactId < 0)
             {
                 throw new ArgumentException(Constant.ErrorMessages.IMAGING_SET_SCHEDULER_ARTIFACT_ID_CANNOT_BE_NEGATIVE);
             }
@@ -31,15 +31,15 @@ namespace KCD_1041539.ImagingSetScheduler.Helper
 
                 using (IObjectManager objectManager = contextContainer.ServicesProxyFactory.CreateServiceProxy<IObjectManager>())
                 {
-                    var queryRequest = NewQueryRequest($"(('Artifact ID' == {imagingSetSchedulerId}))");
+                    var queryRequest = NewQueryRequest($"(('Artifact ID' == {imagingSetSchedulerArtifactId}))");
 
-                    QueryResult queryResult = await objectManager.QueryAsync(workspaceId, queryRequest, 1, batchSize).ConfigureAwait(false);
+                    QueryResult queryResult = await objectManager.QueryAsync(workspaceArtifactId, queryRequest, 1, batchSize).ConfigureAwait(false);
                     totalCount = queryResult.TotalCount;
 
                     if (totalCount > 1)
                     {
                         var errorContext = String.Format("An error has occurred: multiple instances of ImagingSetSchedulerArtifactId: {0}. WorkspaceArtifactId: {1}",
-                                imagingSetSchedulerId, workspaceId);
+                            imagingSetSchedulerArtifactId, workspaceArtifactId);
                         throw new CustomExceptions.ImagingSetSchedulerException(errorContext);
                     }
 
@@ -47,7 +47,7 @@ namespace KCD_1041539.ImagingSetScheduler.Helper
                     {
                         var errorContext =
                             String.Format("An error has occurred: no instance of ImagingSetSchedulerArtifactId: {0}. WorkspaceArtifactId: {1}",
-                                imagingSetSchedulerId, workspaceId);
+                                imagingSetSchedulerArtifactId, workspaceArtifactId);
                         throw new CustomExceptions.ImagingSetSchedulerException(errorContext);
                     }
 
@@ -57,13 +57,13 @@ namespace KCD_1041539.ImagingSetScheduler.Helper
             }
             catch(Exception ex)
             {
-                var errorContext = String.Format("An error occurred when retrieving Imaging Set Scheduler [WorkspaceArtifactId: {0}, ImagingSetSchedulerArtifactId: {1}]", 
-                    workspaceId, imagingSetSchedulerId);
+                var errorContext = String.Format("An error occurred when retrieving Imaging Set Scheduler [WorkspaceArtifactId: {0}, ImagingSetSchedulerArtifactId: {1}]",
+                    workspaceArtifactId, imagingSetSchedulerArtifactId);
                 throw new CustomExceptions.ImagingSetSchedulerException(errorContext, ex);
             }
         }
 
-        public async Task<List<RelativityObject>> RetrieveAllImagingSetSchedulesNotWaitingAsync(int workspaceId, IContextContainer contextContainer)
+        public async Task<List<RelativityObject>> RetrieveAllImagingSetSchedulesNotWaitingAsync(int workspaceArtifactId, IContextContainer contextContainer)
         {
             int batchSize = await contextContainer.InstanceSettingManager.GetIntegerValueAsync("Relativity.Imaging", "ImagingSetSchedulerBatchSize", 1000).ConfigureAwait(false);
             int iterator = 1;
@@ -76,7 +76,7 @@ namespace KCD_1041539.ImagingSetScheduler.Helper
 
                 do
                 {
-                    QueryResult queryResult = await objectManager.QueryAsync(workspaceId, queryRequest, iterator, batchSize).ConfigureAwait(false);
+                    QueryResult queryResult = await objectManager.QueryAsync(workspaceArtifactId, queryRequest, iterator, batchSize).ConfigureAwait(false);
                     totalCount = queryResult.TotalCount;
                     iterator += batchSize;
                     res.AddRange(queryResult.Objects);
@@ -85,7 +85,64 @@ namespace KCD_1041539.ImagingSetScheduler.Helper
                 return res;
             }
         }
+        public async Task<MassUpdateResult> UpdateImagingSetScheduler(int workspaceArtifactId, IContextContainer contextContainer, int imagingSetSchedulerArtifactId, DateTime? lastRun, DateTime? nextRun, string message, string status)
+        {
+            try
+            {
+                MassUpdateResult res;
+                var fieldValuePairs = new List<FieldRefValuePair>
+                {
+                    new FieldRefValuePair()
+                    {
+                        Field = new FieldRef() { Guid = Constant.Guids.Field.ImagingSetScheduler.LAST_RUN },
+                        Value = lastRun
+                    },
+                    new FieldRefValuePair()
+                    {
+                        Field = new FieldRef() { Guid = Constant.Guids.Field.ImagingSetScheduler.NEXT_RUN },
+                        Value = nextRun
+                    },
+                    new FieldRefValuePair()
+                    {
+                        Field = new FieldRef() { Guid = Constant.Guids.Field.ImagingSetScheduler.MESSAGES },
+                        Value = message
+                    },
+                    new FieldRefValuePair()
+                    {
+                        Field = new FieldRef() { Guid = Constant.Guids.Field.ImagingSetScheduler.STATUS },
+                        Value = status
+                    }
+                };
 
+                var updateRequest = new MassUpdateByCriteriaRequest()
+                {
+                    ObjectIdentificationCriteria = new ObjectIdentificationCriteria
+                    {
+                        ObjectType = new ObjectTypeRef { Guid = IMAGING_SET_SCHEDULER },
+                        Condition = $"(('Artifact ID' == {imagingSetSchedulerArtifactId}))"
+                    },
+                    FieldValues = fieldValuePairs,
+                };
+
+                using (IObjectManager objectManager =
+                       contextContainer.ServicesProxyFactory.CreateServiceProxy<IObjectManager>())
+                {
+                    res =
+                        await objectManager.UpdateAsync(workspaceArtifactId, updateRequest).ConfigureAwait(false);
+
+                }
+
+                return res;
+            }
+            catch (Exception ex)
+            {
+                var errorContext = String.Format(
+                    "An error occurred when updating Imaging Set Scheduler [WorkspaceArtifactId: {0}, ImagingSetSchedulerArtifactId: {1}]",
+                    workspaceArtifactId, imagingSetSchedulerArtifactId);
+                throw new CustomExceptions.ImagingSetSchedulerException(errorContext, ex);
+            }
+        }
+    
         private QueryRequest NewQueryRequest(string condition)
         {
             var queryRequest = new QueryRequest()
